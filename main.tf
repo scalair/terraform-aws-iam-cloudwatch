b2784a35d9663787157d5ba7602990bde8e17976
Adel Abdelhak <6298183+blakelead@users.noreply.github.com>
2020-11-26 16:22:57 +0100

    Update iam-user version

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "iam_user" {
-  source = "github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-user?ref=v2.1.0"
+  source  = "github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-user"
+  version = "~> 2.0"
 
   name          = var.iam_user_name
   pgp_key       = var.iam_user_pgp_key
